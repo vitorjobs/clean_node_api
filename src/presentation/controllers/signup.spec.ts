7a0bd3b122e4e90413eda6ee510c406e55f4966f
vitorjobs <vittorjobs@gmail.com>
2022-06-30 11:28:49 -0300

    feat: ensure SignUpController returns 400 if no email is provide

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -13,7 +13,7 @@ describe('TESTE 01 - Nome do componente - SignUP Controller', () =>{
     const sut = new SignUpController()
     const httpRequest = {
       body: {
-        name: 'any name',
+        // name: 'any name',
         email: 'any email',
         password: 'any password',
         passwordConfirmation: 'any password',
@@ -23,5 +23,31 @@ describe('TESTE 01 - Nome do componente - SignUP Controller', () =>{
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+})
+
+/**
+ * TESTE 02
+ * VALIDAÇÕES DO ...
+ * SE EXISTE O ...
+ * SE EXISTE O ...
+ */
+
+ describe('TESTE 02 - Nome do componente - SignUP Controller', () =>{
+  test('Should return 400 if not email is provided', () =>{
+    // sut - sistem undent test
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any name',
+        // email: 'any email',
+        password: 'any password',
+        passwordConfirmation: 'any password',
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
 })
